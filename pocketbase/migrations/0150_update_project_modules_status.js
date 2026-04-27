d6b29b6122c369b6f13ebf7c1ae8e5f604a920ea
Skip <skip@usecurling.com>
2026-04-27 14:20:33 +0000

    feat: Add module grouping and progress tracking

--- pocketbase/migrations/0150_update_project_modules_status.js
+++ pocketbase/migrations/0150_update_project_modules_status.js
@@ -2,16 +2,16 @@ migrate(
   (app) => {
     const col = app.findCollectionByNameOrId('project_modules')
     const statusField = col.fields.getByName('status')
-    if (statusField && !statusField.selectValues.includes('Em Aprovação')) {
-      statusField.selectValues.push('Em Aprovação')
+    if (statusField && !statusField.values.includes('Em Aprovação')) {
+      statusField.values.push('Em Aprovação')
       app.save(col)
     }
   },
   (app) => {
     const col = app.findCollectionByNameOrId('project_modules')
     const statusField = col.fields.getByName('status')
-    if (statusField && statusField.selectValues.includes('Em Aprovação')) {
-      statusField.selectValues = statusField.selectValues.filter((v) => v !== 'Em Aprovação')
+    if (statusField && statusField.values.includes('Em Aprovação')) {
+      statusField.values = statusField.values.filter((v) => v !== 'Em Aprovação')
       app.save(col)
     }
   },
